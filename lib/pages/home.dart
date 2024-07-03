746fd8bc5cfea426cc5d8b0cf9e0bf21cd895541
ShantanuKH <khadseshantanu02@gmail.com>
2024-07-03 15:57:48 +0530

    Created Trending News section

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:news_pulse/model/category_model.dart';
 import 'package:news_pulse/model/slider_model.dart';
 import 'package:news_pulse/services/data.dart';
@@ -30,6 +31,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         title: RichText(
           text: TextSpan(
@@ -50,105 +52,225 @@ class _HomePageState extends State<HomePage> {
         elevation: 1,
         centerTitle: true,
       ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.only(left: 10),
-              height: 70,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    return CatergoryTiles(
-                      image: categories[index].image,
-                      catergoryName: categories[index].catergoryName,
-                    );
-                  }),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-             Padding(
-              padding: EdgeInsets.only(left: 10,right: 10),
-               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Breaking News !",
-                 style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'PlaywriteGBS'
-                 ),
-                 ),
-                 Text("View All",
-                 style: TextStyle(
-                  color: Colors.redAccent,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w600
-                 ),
-                 ),
-                ],
-               ),
-             ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                height: 70,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: categories.length,
+                    itemBuilder: (context, index) {
+                      return CatergoryTiles(
+                        image: categories[index].image,
+                        catergoryName: categories[index].catergoryName,
+                      );
+                    }),
+              ),
               SizedBox(
-              height: 15,
-            ),
-
-
-            CarouselSlider.builder(
-                itemCount: slider.length,
-                itemBuilder: (context, index, realIndex) {
-                  String? res = slider[index].image;
-                  String? sname = slider[index].name;
-                  return buildImage(res!, index, sname!);
-                },
-               
-                options: CarouselOptions(
-                    height: 240,
-                    autoPlay: true,
-                    autoPlayAnimationDuration: Durations.long4,
-                    enlargeCenterPage: true,
-                    enlargeStrategy: CenterPageEnlargeStrategy.zoom,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        activeIndex = index;
-                      });
-                    })),
-            SizedBox(
-              height: 20,
-            ),
-            Center(child: buildIndicator()),
-              SizedBox(
-              height: 20,
-            ),
-                         Padding(
-              padding: EdgeInsets.only(left: 10,right: 10),
-               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Trending News !",
-                 style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'ZillaSlab'
+                height: 15,
+              ),
+               Padding(
+                padding: EdgeInsets.only(left: 10,right: 10),
+                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Breaking News !",
+                   style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'PlaywriteGBS'
+                   ),
+                   ),
+                   Text("View All",
+                   style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600
+                   ),
+                   ),
+                  ],
                  ),
-                 ),
-                 Text("View All",
-                 style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w600
-                 ),
-                 ),
-                ],
                ),
-             ),
-          ],
+                SizedBox(
+                height: 15,
+              ),
+        
+        
+              CarouselSlider.builder(
+                  itemCount: slider.length,
+                  itemBuilder: (context, index, realIndex) {
+                    String? res = slider[index].image;
+                    String? sname = slider[index].name;
+                    return buildImage(res!, index, sname!);
+                  },
+                 
+                  options: CarouselOptions(
+                      height: 240,
+                      autoPlay: true,
+                      autoPlayAnimationDuration: Durations.long4,
+                      enlargeCenterPage: true,
+                      enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          activeIndex = index;
+                        });
+                      })),
+              SizedBox(
+                height: 20,
+              ),
+              Center(child: buildIndicator()),
+                SizedBox(
+                height: 20,
+              ),
+                           Padding(
+                padding: EdgeInsets.only(left: 10,right: 10),
+                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Trending News !",
+                   style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'ZillaSlab'
+                   ),
+                   ),
+                   Text("View All",
+                   style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600
+                   ),
+                   ),
+                  ],
+                 ),
+               ),
+               SizedBox(
+                height: 5,
+              ),
+               Padding(
+                 padding: const EdgeInsets.only(left:7,right: 7),
+                 child: Material(
+                  elevation: 5,
+                  borderRadius: BorderRadius.circular(10),
+                   child: Padding(
+                     padding: const EdgeInsets.symmetric(vertical: 10),
+                     child: Row(
+                       crossAxisAlignment:CrossAxisAlignment.start,
+                       
+                      children: [
+                        Container(
+                          child:ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset("assets/business.jpeg",
+                            height:150 ,
+                            width: 150,
+                            fit: BoxFit.cover,
+                            ),
+                          )
+                        ),
+                         SizedBox(
+                                   width: 12,
+                                 ),
+                         Column(
+                           children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width/2,
+                               child: Text("T20 WorldCup 2024",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600
+                              ),
+                              ),
+                             ),
+                              SizedBox(
+                               height: 4,
+                               ),
+                             Container(
+                              width: MediaQuery.of(context).size.width/2,
+                               child: Text("India Men Cricket Team Won the T20 WorldCup 2024",
+                              style: TextStyle(
+                                color: Colors.black38,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600
+                              ),
+                              ),
+                             ),
+                           ],
+                         ),
+                     ],
+                     ),
+                   ),
+                 ),
+               ),
+                SizedBox(
+                               height: 15,
+                               ),
+                Padding(
+                 padding: const EdgeInsets.only(left:7,right: 7),
+                 child: Material(
+                  elevation: 5,
+                  borderRadius: BorderRadius.circular(10),
+                   child: Padding(
+                     padding: const EdgeInsets.symmetric(vertical: 10),
+                     child: Row(
+                       crossAxisAlignment:CrossAxisAlignment.start,
+                       
+                      children: [
+                        Container(
+                          child:ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset("assets/sports.jpeg",
+                            height:150 ,
+                            width: 150,
+                            fit: BoxFit.cover,
+                            ),
+                          )
+                        ),
+                         SizedBox(
+                                   width: 12,
+                                 ),
+                         Column(
+                           children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width/2,
+                               child: Text("T20 WorldCup 2024",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600
+                              ),
+                              ),
+                             ),
+                              SizedBox(
+                               height: 4,
+                               ),
+                             Container(
+                              width: MediaQuery.of(context).size.width/2,
+                               child: Text("India Men Cricket Team Won the T20 WorldCup 2024",
+                              style: TextStyle(
+                                color: Colors.black38,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600
+                              ),
+                              ),
+                             ),
+                           ],
+                         ),
+                     ],
+                     ),
+                   ),
+                 ),
+               )
+            ],
+          ),
         ),
       ),
     );
